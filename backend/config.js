c0a9d464466cdde04b2bc66955e687385fcab937
Shreyas <shreyas@Shreyass-MacBook-Air.local>
2024-08-03 12:16:38 +0530

    vercel

--- backend/config.js
+++ backend/config.js
@@ -5,12 +5,8 @@ dotenv.config();
 
 const { Pool} = pg;
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
+  const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
   });
 
 export default pool;
