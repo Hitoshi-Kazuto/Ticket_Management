48f7ff8fea3b4f55c742a7567df7eafb0015a8c9
Shreyas <shreyas@Shreyass-MacBook-Air.local>
2024-09-12 16:31:12 +0530

    corrections

--- backend/config.js
+++ backend/config.js
@@ -6,15 +6,10 @@ dotenv.config();
 const { Pool} = pg;
 
 const pool = new Pool({
-  // connectionString: process.env.POSTGRES_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  databse: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  connectionString: process.env.POSTGRES_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
