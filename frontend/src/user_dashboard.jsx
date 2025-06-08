94a00777a8b405407ca59502be4da29cc1356f65
Shreyas <shreyas.dbg@gmail.com>
2025-06-09 01:11:46 +0530

    Table Changes

--- frontend/src/user_dashboard.jsx
+++ frontend/src/user_dashboard.jsx
@@ -14,7 +14,7 @@ const dashboard = () => {
     return (
         <div>
             <Home />
-            <div className='overflow-x-auto shadow-md absolute right-0 w-5/6 bg-white border border-gray-200 p-4 m-0 text-2xl font-bold text-gray-700'>User Dashboard</div>
+            <div className='overflow-x-auto shadow-md absolute right-0 w-5/6 bg-gray-200 border border-gray-200 p-4 m-0 text-2xl font-bold text-gray-700'>User Dashboard</div>
         </div>
     )
 }
