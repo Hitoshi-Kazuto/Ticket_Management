9cf251397480db9cc2f13b4d0e42cd0c3b97633d
Shreyas <shreyas.dbg@gmail.com>
2025-06-10 03:18:47 +0530

    Header

--- frontend/src/components/Home/header.jsx
+++ frontend/src/components/Home/header.jsx
@@ -71,8 +71,8 @@ const Header = () => {
     };
 
     return (
-        <nav className="bg-gray-200 h-16 fixed top-0 left-0 w-full z-50 flex items-center">
-            <div className="flex justify-between items-center w-full mx-4 my-0 max-w-full">
+        <nav className="bg-gray-200 h-16 relative my-0 z-50 flex items-center">
+            <div className="flex justify-between items-center w-full mx-4 my-0 max-w">
                 <a href="#" className="flex items-center">
                     <img src={logo} className="mr-3 h-7 sm:h-9" alt="Logo" />
                     <span className="self-right text-2xl font-bold whitespace-nowrap text-gray-700">Helpdesk Management</span>
--- frontend/src/components/Home/home.jsx
+++ frontend/src/components/Home/home.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import Header from "./header"
 import Sidebar from "./sidebar_select"
 
-const Home = ({ children }) => {
+const Home = () => {
   return (
-    <div className="relative h-screen bg-gray-100">
+    <div className="flex">
       <Sidebar />
-      <Header />
-      <div className="absolute top-16 left-1/6 right-0 bottom-0 overflow-y-auto p-4">
-        {children}
+      <div className="flex flex-col w-full">
+        <Header />
       </div>
     </div>
   );
