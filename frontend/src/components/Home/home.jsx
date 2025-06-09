e5f5505726769a73aa0832b9dd2bb1d336d57ef2
Shreyas <shreyas.dbg@gmail.com>
2025-06-10 03:10:44 +0530

    Header

--- frontend/src/components/Home/home.jsx
+++ frontend/src/components/Home/home.jsx
@@ -4,9 +4,9 @@ import Sidebar from "./sidebar_select"
 
 const Home = ({ children }) => {
   return (
-    <div className="flex h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="flex h-screen bg-gray-100">
       <Sidebar />
-      <div className="flex flex-col flex-1 overflow-hidden">
+      <div className="flex flex-col flex-1">
         <Header />
         <main className="flex-1 overflow-y-auto p-4">
           {children}
