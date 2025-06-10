35fca9d8b45517ce2c0aaa383ebcc669030107b8
Shreyas <shreyas.dbg@gmail.com>
2025-06-10 16:56:38 +0530

    Few Error Fixs

--- frontend/src/masters/ticket_master.jsx
+++ frontend/src/masters/ticket_master.jsx
@@ -293,7 +293,7 @@ const TicketMaster = () => {
                     onClick={(e) => handleShowUpdates(e, row.ticket_id)}
                     className="text-gray-700 hover:text-blue-700 transition-colors duration-200"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                     </svg>
                 </button>
@@ -308,7 +308,7 @@ const TicketMaster = () => {
                           <button
                               title='Withdraw'
                               onClick={(e) => handleWithdraw(e, row.ticket_id)}
-                              className="text-gray-700 hover:text-gray-900 transition-colors duration-200 ml-2"
+                              className="text-gray-700 hover:text-yellow-700 transition-colors duration-200 ml-2"
                           >
                               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10">
                                   <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
